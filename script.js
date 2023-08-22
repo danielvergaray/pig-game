bbde249b1be6bd9acc5b98cdd38cd713944788f8
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-08-22 17:33:44 -0500

    falta la logica de reiniciar el juego y mejorar codigo

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function hold (){
 ////////////////////////////////////
 
 
-let playing
+/* let playing
 
 
 
@@ -71,21 +71,19 @@ let numeroAlAzar = 0
 
 let currentPlayer= "playerOne"
 let dadoAcumuladoPlayer1=0
-let dadoAcumuladoPlayer2=0
+let dadoAcumuladoPlayer2=0 */
 
 
-if(currentPlayer==="playerOne"){
+/* if(currentPlayer==="playerOne"){
     player1()
 }
 else{
     player2()
-}
+} */
 
 
-function player1() {
-    /* playing= true */
-
-    /* currentPlayer="playerOne" */
+/* function player1() {
+   
 
     let namePlayer= "playerOne";
     let botonRollDice = document.getElementById("rollBtnHtml");
@@ -96,10 +94,10 @@ function player1() {
 
     dadoAcumuladoPlayer1=0
     
-}
+} */
 
 // Acumular puntos
-function acumularPuntos() {
+/* function acumularPuntos() {
 
     if (currentPlayer === "playerOne") {
         let currentScore = document.getElementById("currentScoreHtml");
@@ -114,9 +112,7 @@ function acumularPuntos() {
             currentScore.textContent = "21+";
         }
 
-       /*  if (currentPlayer === "playerOne") {
-            currentPlayer = "playerTwo";
-        } */
+       
     } else {
         let currentScore = document.getElementById("currentScoreHtml2");
 
@@ -130,11 +126,8 @@ function acumularPuntos() {
             currentScore.textContent = "21+";
         }
 
-       /*  if (currentPlayer === "playerTwo") {
-            currentPlayer = "playerOne";
-        } */
     }
-}
+} */
 
 
 
@@ -143,19 +136,19 @@ let dadoAcumulado = 0
 
 
 
-function player2() {
+/* function player2() {
     let namePlayer= "playerTwo"
 
     currentPlayer="playerTwo"
 
     let botonRollDice = document.getElementById("rollBtnHtml");
     botonRollDice.addEventListener("click", acumularPuntos );
-    /* console.log("player 2") */
+  
     let botonHold = document.getElementById("holdHtml");
     botonHold.addEventListener("click", ()=> hold (namePlayer))
 
     dadoAcumuladoPlayer2=0
-}
+} */
 
 
 
@@ -163,12 +156,12 @@ function player2() {
 
 
 
-function lanzarDado() {
+/* function lanzarDado() {
     numeroAlAzar = Math.trunc(Math.random() * 6) + 1;
 
-    /* acumularPuntos(); */
+   
     return numeroAlAzar;
-}
+} */
 
 
 
@@ -178,12 +171,12 @@ function lanzarDado() {
 
 
 
-function hold(namePlayer) {
+/* function hold(namePlayer) {
     
     if(namePlayer==="playerOne"){
 
         let highscore = document.getElementById("highscoreHtml");
-        /* let dadoAcumulado=acumularPuntos(); */
+       
     
         let jugador1Html = document.getElementById("player1Container");
         jugador1Html.classList.toggle("player-container-current")
@@ -201,7 +194,7 @@ function hold(namePlayer) {
     }
     else{
         let highscore = document.getElementById("highscoreHtml2");
-        /* let dadoAcumulado=acumularPuntos(); */
+    
     
         let jugador1Html = document.getElementById("player1Container");
         jugador1Html.classList.toggle("player-container-current")
@@ -221,4 +214,95 @@ function hold(namePlayer) {
 
 
 
+} */
+
+
+// capturo elementos del html
+let score0=document.getElementById("currentScoreHtml--0");
+let score1=document.getElementById("currentScoreHtml--1");
+let highScore0= document.getElementById("highScoreHtml--0");
+let highScore1= document.getElementById("highScoreHtml--1");
+let btnDado=document.getElementById("rollBtnHtml");
+let dadoFisico= document.getElementById("figuraDado");
+let dadoFisicoContainer= document.getElementById("dadoId");
+let player1Container= document.getElementById("playerContainer--0");
+let player2Container= document.getElementById("playerContainer--1");
+let btnHold= document.getElementById("holdHtml");
+
+//Valores iniciales
+score0.textContent=0;
+score1.textContent=0;
+let playing=true;
+/* highScore0=0;
+highScore1=0; */
+
+let scores= [0,0]; // Aqui se guardarán los scores de los jugadores
+let currentScore=0;
+let activePlayer=0; //Estamos manejando un sistema donde player 0 es jugador 1 y player 1 es jugador 2
+
+
+//Lanzamiento del dado
+btnDado.addEventListener("click", function(){
+    if(playing){
+        let resultadoDado=Math.trunc(Math.random()*6)+1;
+        //Mostrar figura del dado
+        dadoFisico.classList.remove("oculto")
+        dadoFisico.src = `./imagenes/dice-${resultadoDado}.png`; 
+        /* Mediante .src se puede modificar la ruta de la imagen, lo cual 
+        hace que la imagen a mostrar sea dinamica, es decir, que se pueda modificar el 
+        nombre de la imagen a mostrar */
+    
+        if(resultadoDado!==1){
+            currentScore+=resultadoDado;
+            
+            document.getElementById(`currentScoreHtml--${activePlayer}`).textContent=currentScore; 
+            /* Tal como se hace con el dado, se usan temple strings  para que se edite el currentScore
+            de manera dinamica de acuerdo a que jugador este jugando */
+        } else {
+                
+            cambiarJugador()
+        }
+    }
+    
+    
+});
+
+btnHold.addEventListener("click", hold)
+
+function hold (){
+    if(playing){
+        scores[activePlayer]+=currentScore; //scores es un array, y active player marcará si juega player 1(0) o player 2(1)
+
+        document.getElementById(`highScoreHtml--${activePlayer}`).textContent=scores[activePlayer];
+    
+        if(scores[activePlayer]>=20){
+            playing=false;
+            document.getElementById(`playerContainer--${activePlayer}`).classList.add("player-ganador");
+            document.getElementById(`playerContainer--${activePlayer}`).classList.remove("player-container-current");
+            dadoFisicoContainer.classList.add("oculto")
+        } else{
+            cambiarJugador()
+        }
+    }
+    
 }
+
+function cambiarJugador(){
+    document.getElementById(`currentScoreHtml--${activePlayer}`).textContent=0;
+    currentScore=0;
+    if(activePlayer===0){
+        activePlayer=1;
+    } else{
+        activePlayer=0;
+    }
+// Cambio el color del jugador activo
+player1Container.classList.toggle("player-container-current");
+player2Container.classList.toggle("player-container-current"); 
+}
+// SI el numero del dado es igual a 1, el jugador pierde puntos y el turno pasa al otro
+
+
+
+
+
+
